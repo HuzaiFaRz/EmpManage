fd42467773faa039d268902459c2dc9f718524a8
HuzaiFa Raza <huziafarazaa999@gmail.com>
2024-11-22 13:57:33 +0500

    Employee Form 80%

--- src/Auth/AuthHeading.jsx
+++ src/Auth/AuthHeading.jsx
@@ -1,7 +1,8 @@
 const AuthHeading = ({ HeadingText }) => {
   return (
-    <h1 className="font-semibold tracking-tighter text-6xl">
+    <h1 className="font-semibold tracking-tighter text-6xl w-[300px]">
       {HeadingText}
+      
     </h1>
   );
 };
--- src/Auth/EmployeeForm.jsx
+++ src/Auth/EmployeeForm.jsx
@@ -1,176 +1,132 @@
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import AuthHeading from "./AuthHeading";
-import Flatpickr from "react-flatpickr";
 import { DefaultProfilePic, ThemeDarkToLight } from "../Main_Components/App";
-import Select from "react-select";
 
 const EmployeeForm = () => {
-  const [dateState, setDateState] = useState({
-    date1: new Date(),
-    date2: new Date(),
-  });
-  const [employeeProfessionSelect, setEmployeeProfessionSelect] =
-    useState(null);
+  const EmployeeInput =
+    "p-2 bg-transparent border border-lightBg color-lightBg font-light capitalize tracking-[1px] placeholder:text-lightBg focus:outline-0 dark:border-darkBg w-[300px]  dark:placeholder:text-darkBg";
+  const EmployeeLabel =
+    "flex flex-col items-start justify-center gap-2 font-light";
 
-  console.log(employeeProfessionSelect);
   return (
     <Fragment>
-      <div className="Employee_Page w-full h-screen flex flex-col justify-evenly items-center">
+      <div className="Employee_Page w-full h-screen flex flex-col justify-evenly items-center p-1">
         <form
-          className={`Employee_Form flex flex-wrap items-center justify-evenly gap-3 w-[1000px] h-[600px] p-2 ${ThemeDarkToLight}`}
+          className={`Employee_Form flex flex-wrap items-center justify-evenly gap-4 w-[1000px] max-w-full h-[600px] min-h-max p-2 ${ThemeDarkToLight}`}
         >
           <AuthHeading HeadingText={"Employee"} />
-          <label
-            htmlFor="Employee_First_Name"
-            className="flex flex-col items-start justify-center gap-2"
-          >
+          <label htmlFor="Employee_First_Name" className={EmployeeLabel}>
             Enter First Name
             <input
               type="text"
               placeholder="First Name"
               id="Employee_First_Name"
-              className="p-2 bg-transparent border-2 border-lightBg color-lightBg font-semibold capitalize tracking-[1px] placeholder:text-lightBg focus:outline-0"
+              className={EmployeeInput}
             />
           </label>
-          <label
-            htmlFor="Employee_Last_Name"
-            className="flex flex-col items-start justify-center gap-2"
-          >
+          <label htmlFor="Employee_Last_Name" className={EmployeeLabel}>
             Enter Last Name
             <input
               type="text"
               placeholder="Last Name"
               id="Employee_Last_Name"
-              className="p-2 bg-transparent border-2 border-lightBg color-lightBg font-semibold capitalize tracking-[1px] placeholder:text-lightBg focus:outline-0"
+              className={EmployeeInput}
             />
           </label>
-          <label
-            htmlFor="Employee_Email"
-            className="flex flex-col items-start justify-center gap-2"
-          >
+          <label htmlFor="Employee_Email" className={EmployeeLabel}>
             Enter Email
             <input
               type="email"
               placeholder="Email"
               id="Employee_Email"
-              className="p-2 bg-transparent border-2 border-lightBg color-lightBg font-semibold capitalize tracking-[1px] placeholder:text-lightBg focus:outline-0"
+              className={EmployeeInput}
             />
           </label>
-          <label
-            htmlFor="Employee_Phone_Number"
-            className="flex flex-col items-start justify-center gap-2"
-          >
+          <label htmlFor="Employee_Phone_Number" className={EmployeeLabel}>
             Enter Phone Number
             <input
               type="number"
               placeholder="Phone Number"
               id="Employee_Phone_Number"
-              className="p-2 bg-transparent border-2 border-lightBg color-lightBg font-semibold capitalize tracking-[1px] placeholder:text-lightBg focus:outline-0"
+              className={EmployeeInput}
             />
           </label>
 
-          <label
-            htmlFor="Employee_Address"
-            className="flex flex-col items-start justify-center gap-2"
-          >
+          <label htmlFor="Employee_Address" className={EmployeeLabel}>
             Enter Address
             <input
               type="text"
               placeholder="Address"
               id="Employee_Address"
-              className="p-2 bg-transparent border-2 border-lightBg color-lightBg font-semibold capitalize tracking-[1px] placeholder:text-lightBg focus:outline-0"
+              className={EmployeeInput}
             />
           </label>
-          <label
-            htmlFor="Employee_City"
-            className="flex flex-col items-start justify-center gap-2"
-          >
+          <label htmlFor="Employee_City" className={EmployeeLabel}>
             Enter City
             <input
               type="text"
               placeholder="City"
               id="Employee_City"
-              className="p-2 bg-transparent border-2 border-lightBg color-lightBg font-semibold capitalize tracking-[1px] placeholder:text-lightBg focus:outline-0"
+              className={EmployeeInput}
             />
           </label>
-          <label
-            htmlFor="Employee_State"
-            className="flex flex-col items-start justify-center gap-2"
-          >
+          <label htmlFor="Employee_State" className={EmployeeLabel}>
             Enter State
             <input
               type="text"
               placeholder="State"
               id="Employee_State"
-              className="p-2 bg-transparent border-2 border-lightBg color-lightBg font-semibold capitalize tracking-[1px] placeholder:text-lightBg focus:outline-0"
+              className={EmployeeInput}
             />
           </label>
-          <label
-            htmlFor="Employee_BirthDay"
-            className="flex flex-col items-start justify-center gap-2"
-          >
-            Date of Birth
-            <Flatpickr
-              value={dateState.date}
-              onChange={([date1]) => {
-                setDateState({ date1 });
-              }}
-              id="Employee_BirthDay"
-              placeholder="Date of Birth"
-              className="p-2 bg-transparent border-2 border-lightBg color-lightBg font-semibold capitalize tracking-[1px] placeholder:text-lightBg focus:outline-0"
+          <label htmlFor="Employee_Age" className={EmployeeLabel}>
+            Age
+            <input
+              type="number"
+              placeholder="Age"
+              id="Employee_Age"
+              className={EmployeeInput}
             />
           </label>
 
-          <div className="flex flex-col justify-center items-center">
-            <label
-              htmlFor="Empolyee_Profession"
-              className="flex flex-col items-start justify-center gap-2"
-            >
-              Profession
-              <Select
-                isSearchable={false}
-                isClearable={true}
-                id="Empolyee_Profession"
-                defaultValue={null}
-                onChange={setEmployeeProfessionSelect}
-                options={[
-                  { value: "Web Developer", label: "Web Developer" },
-                  { value: "App Developer", label: "App Developer" },
-                  { value: "Graphic Designer", label: "Graphic Designer" },
-                  { value: "Content Writer", label: "Content Writer" },
-                ]}
-              />
-            </label>
-            <label
-              htmlFor="Empolyee_Profession"
-              className="flex flex-col items-start justify-center gap-2"
-            >
-              Experience
-              <Select
-                isSearchable={false}
-                isClearable={true}
-                id="Empolyee_Profession"
-                defaultValue={null}
-                onChange={setEmployeeProfessionSelect}
-                options={[
-                  { value: "Junior", label: "Junior" },
-                  { value: "Senior", label: "Senior" },
-                ]}
-              />
-            </label>
-          </div>
+          <label htmlFor="Empolyee_Profession" className={EmployeeLabel}>
+            Profession
+            <input
+              type="text"
+              placeholder="Profession"
+              id="Empolyee_Profession"
+              className={EmployeeInput}
+            />
+          </label>
 
-          <label
-            htmlFor="Employee_Profile_Pic"
-            className="flex flex-col items-start justify-center gap-2"
-          >
+          <label htmlFor="Empolyee_Experience" className={EmployeeLabel}>
+            Experience
+            <input
+              type="text"
+              placeholder="Experience"
+              id="Empolyee_Experience"
+              className={EmployeeInput}
+            />
+          </label>
+
+          <label htmlFor="Employee_Profile_Pic" className={EmployeeLabel}>
             <img src={DefaultProfilePic} className="w-[100px] h-[100px]" />
+            <button className="Employee_Profile_Pic">
+              Select Profile Photo
+            </button>
             <input
               type="file"
               id="Employee_Profile_Pic"
-              accept=".png .jpg .jpeg .webp"
+              accept="image/*"
+              className="hidden"
             />
           </label>
+
+          <input
+            type="submit"
+            id="Employee_Form_Submit_Button"
+            className="cursor-pointer bg-lightBg text-lightText border-0 px-[15px] py-[8px] rounded-[20px] text-1xl"
+          />
         </form>
       </div>
     </Fragment>
