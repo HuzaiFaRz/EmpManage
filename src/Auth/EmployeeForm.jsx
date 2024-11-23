09bf4320e4b8faefb7a3fa14e0b0a4d8f468447f
HuzaiFa Raza <huziafarazaa999@gmail.com>
2024-11-23 10:45:53 +0500

    Employee Form UI Functionility 90%

--- src/Auth/EmployeeForm.jsx
+++ src/Auth/EmployeeForm.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import React, { Fragment, useState } from "react";
 import AuthHeading from "./AuthHeading";
 import { ThemeDarkToLight } from "../Main_Components/App";
 import { useForm } from "react-hook-form";
@@ -19,11 +19,40 @@ const EmployeeForm = () => {
   const EmployeeLabel =
     "flex flex-col items-start justify-center gap-2 font-light";
   const [employeeSubmitLoading, setEmployeeSubmitLoading] = useState(false);
+  const inputs = [
+    { ID: "Employee_First_Name", Placeholder: "First Name", Type: "text" },
+    { ID: "Employee_Last_Name", Placeholder: "Last Name", Type: "text" },
+    { ID: "Employee_Email", Placeholder: "Email", Type: "email" },
+    {
+      ID: "Employee_Phone_Number",
+      Placeholder: "Phone Number",
+      Type: "number",
+    },
+    { ID: "Employee_Address", Placeholder: "Address", Type: "text" },
+    { ID: "Employee_City", Placeholder: "City", Type: "text" },
+    { ID: "Employee_State", Placeholder: "State", Type: "text" },
+    { ID: "Employee_Age", Placeholder: "Age", Type: "number" },
+    { ID: "Empolyee_Profession", Placeholder: "Profession", Type: "text" },
+    {
+      ID: "Empolyee_Experience",
+      Placeholder: "Experience Year",
+      Type: "number",
+    },
+  ];
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setError,
+    formState: { errors },
+  } = useForm();
 
-  const { register, handleSubmit, reset } = useForm();
+  console.log(errors);
+
   const Employee_Form_Handler = async (employee_Data) => {
     try {
       event.preventDefault();
+      console.log(employee_Data);
       setEmployeeSubmitLoading(true);
       const employeDataAdding = await addDoc(
         collection(db, "Employees"),
@@ -49,118 +78,40 @@ const EmployeeForm = () => {
           onSubmit={handleSubmit(Employee_Form_Handler)}
         >
           <AuthHeading HeadingText={"Employee"} />
-          <label htmlFor="Employee_First_Name" className={EmployeeLabel}>
-            Enter First Name
-            <input
-              type="text"
-              placeholder="First Name"
-              id="Employee_First_Name"
-              className={EmployeeInput}
-              {...register("Employee_First_Name", {
-                required: true,
-              })}
-            />
-          </label>
-          <label htmlFor="Employee_Last_Name" className={EmployeeLabel}>
-            Enter Last Name
-            <input
-              type="text"
-              placeholder="Last Name"
-              id="Employee_Last_Name"
-              className={EmployeeInput}
-              {...register("Employee_Last_Name", { required: true })}
-            />
-          </label>
-          <label htmlFor="Employee_Email" className={EmployeeLabel}>
-            Enter Email
-            <input
-              type="email"
-              placeholder="Email"
-              id="Employee_Email"
-              className={EmployeeInput}
-              {...register("Employee_Email", { required: true })}
-            />
-          </label>
-          <label htmlFor="Employee_Phone_Number" className={EmployeeLabel}>
-            Enter Phone Number
-            <input
-              type="number"
-              placeholder="Phone Number"
-              id="Employee_Phone_Number"
-              className={EmployeeInput}
-              {...register("Employee_Phone_Number", { required: true })}
-            />
-          </label>
 
-          <label htmlFor="Employee_Address" className={EmployeeLabel}>
-            Enter Address
-            <input
-              type="text"
-              placeholder="Address"
-              id="Employee_Address"
-              className={EmployeeInput}
-              {...register("Employee_Address", { required: true })}
-            />
-          </label>
-          <label htmlFor="Employee_City" className={EmployeeLabel}>
-            Enter City
-            <input
-              type="text"
-              placeholder="City"
-              id="Employee_City"
-              className={EmployeeInput}
-              {...register("Employee_City", { required: true })}
-            />
-          </label>
-          <label htmlFor="Employee_State" className={EmployeeLabel}>
-            Enter State
-            <input
-              type="text"
-              placeholder="State"
-              id="Employee_State"
-              className={EmployeeInput}
-              {...register("Employee_State", { required: true })}
-            />
-          </label>
-          <label htmlFor="Employee_Age" className={EmployeeLabel}>
-            Enter Age
-            <input
-              type="number"
-              placeholder="Age"
-              id="Employee_Age"
-              className={EmployeeInput}
-              {...register("Employee_Age", { required: true })}
-            />
-          </label>
-
-          <label htmlFor="Empolyee_Profession" className={EmployeeLabel}>
-            Enter Profession
-            <input
-              type="text"
-              placeholder="Profession"
-              id="Empolyee_Profession"
-              className={EmployeeInput}
-              {...register("Empolyee_Profession", { required: true })}
-            />
-          </label>
-
-          <label htmlFor="Empolyee_Experience" className={EmployeeLabel}>
-            Enter Experience
-            <input
-              type="number"
-              placeholder="Experience Year"
-              id="Empolyee_Experience"
-              className={EmployeeInput}
-              {...register("Empolyee_Experience", { required: true })}
-            />
-          </label>
-
+          {inputs.map((elem, index) => {
+            const { ID, Placeholder, Type } = elem;
+            return (
+              <React.Fragment key={index}>
+                <label htmlFor={ID} className={EmployeeLabel}>
+                  Enter {Placeholder}
+                  <input
+                    type={Type}
+                    placeholder={Placeholder}
+                    id={Placeholder}
+                    className={EmployeeInput}
+                    {...register(ID, {
+                      required: `${Placeholder} is required.`,
+                    })}
+                  />
+                  {errors[ID] && (
+                    <p className="text-[red] font-normal text-[13px] tracking-wider">
+                      {errors?.[ID]?.message}
+                    </p>
+                  )}
+                </label>
+              </React.Fragment>
+            );
+          })}
           <div className="w-full p-2 m-2 flex items-center justify-center">
             <button
               type="submit"
               className="cursor-pointer bg-colorOne dark:bg-colorTwo text-colorTwo dark:text-colorOne border-0 px-[15px] py-[8px] rounded-[20px] text-1xln hover:translate-y-1 transition-all flex justify-center items-center gap-5"
               id="Employee_Form_Submit_Button"
               disabled={employeeSubmitLoading && true}
+              onClick={() => {
+                setError("test", { type: "focus" }, { shouldFocus: true });
+              }}
             >
               <span>Add Employee</span>
               {employeeSubmitLoading ? (
