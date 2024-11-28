5449164dfefc603e21657826acd8e4cb254a4e2c
HuzaiFa Raza <huziafarazaa999@gmail.com>
2024-11-28 14:07:04 +0500

    Create_Employee_Form Routes Created

--- src/Employee/Employee_Heading.jsx
+++ src/Employee/Employee_Heading.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line react/prop-types
 const Employee_Heading = ({ HeadingText }) => {
   return (
     <h1 className="font-semibold tracking-tighter text-4xl w-[100%]  py-2 text-center">
--- src/Main_Components/App.jsx
+++ src/Main_Components/App.jsx
@@ -1,18 +1,24 @@
 import Navbar from "../Navbar/Navbar";
 import DefaultProfilePic from "../assets/Images/Default_Profile_Pic.jpg";
 import Create_Employee_Form from "../Employee/Create_Employee_Form";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 const ThemeLightToDark =
   "bg-colorOne dark:bg-colorTwo text-colorTwo dark:text-colorOne";
 const ThemeDarkToLight =
   "bg-colorTwo text-colorOne dark:bg-colorOne dark:text-colorTwo";
 const App = () => {
   return (
-    <div
-      className={`App transition-all flex flex-col justify-between items-center`}
-    >
-      <Navbar />
-      <Create_Employee_Form />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/create_employee" element={<Create_Employee_Form />} />
+      </Routes>
+    </BrowserRouter>
+    // <div
+    //   className={`App transition-all flex flex-col justify-between items-center`}
+    // >
+    //   <Navbar />
+    //   <Create_Employee_Form />
+    // </div>
   );
 };
 
